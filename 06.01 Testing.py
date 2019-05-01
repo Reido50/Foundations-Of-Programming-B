42662e7a0c2b7f8aae8d5278ce38ddd85c4e012d
19harre <19harrre@stu.kingsford.org>
2019-05-01 11:10:58 -0500

    completed coding and careers

--- 06.01 Testing.py
+++ 06.01 Testing.py
@@ -6,7 +6,7 @@ def main():
     print("Inches to Centimeters Conversion:")
       
     for x in range(3):
-        inch = float(input("Enter number of inches for trial " + str(x + 1) + ": "))
+        inch = abs(float(input("Enter number of inches for trial " + str(x + 1) + ": ")))
         cm = inch * 2.54
         print(str(inch) + " inches is equal to " + str(cm) + " centimeters.")
 main()
--- 06.03 Coding and Careers.py
+++ 06.03 Coding and Careers.py
@@ -3,20 +3,79 @@
 # 4/29/2019
 # Converts between length units based on user input
 
+from math import *
+
+lengthUnits = ["cm", "in", "ft", "yd", "m", "km", "mi"]
+lengthConversions = [1.0, 2.54, 12.0, 3.0, 1.09361, 1000.0, 1.60934]
+lengthUnitPos = -1
+newLengthUnitPos = 0
+userUnit = ""
+userVal = 0.0
+
+def convert(old, new):
+    global lengthConversions
+    global userVal 
+    
+    if(old != -1):
+        increment = int(copysign(1.0, new - old))
+        for x in range(int(old), int(new), increment):
+            if(increment > 0):
+                userVal/=lengthConversions[x+increment]
+            elif(increment < 0):
+                userVal*=lengthConversions[x]
+
+def promptUnit():
+    global lengthUnits
+    global lengthUnitPos
+    global newLengthUnitPos
+    global userUnit
+
+    while(userUnit == ""):
+        # Prompt user for a length unit
+        if(lengthUnitPos == -1):
+            userUnit = input("Which unit of length would you like to start with (cm, in, ft, yd, m, km, mi)? Enter 'stop' to quit program:").lower()
+        else:
+            userUnit = input("Which unit of length would you like to convert to (cm, in, ft, yd, m, km, mi)? Enter 'stop' to quit program:").lower()
+
+        # Check for stop
+        if(userUnit == "stop"):
+            print("Thank you for using the Length Unit Coversion Calculator!")
+            quit()
+
+        # Check for length unit in constant list
+        for u in range(len(lengthUnits)):
+            if(lengthUnits[u] == userUnit):   # Length unit found
+                newLengthUnitPos = u
+                convert(lengthUnitPos, newLengthUnitPos)
+                lengthUnitPos = u
+                break
+            elif(u == len(lengthUnits) - 1):  # Length unit not found
+                userUnit = ""
+                print("The unit you chose was not found. Try again.")
+
+def promptMeasure():
+    global userVal
+    global userUnit
+
+    userVal = abs(float(input("Enter the measurement you would like to use (" + str(userUnit) + "):")))
+
 def main():
+    global userUnit
+
     print("Length Unit Conversion Calculator")
     print("---------------------------------")
 
-    lengthUnits = ["cm", "in", "ft", "yd", "m", "km", "mi"]
-    lengthUnitPos = 0
-    measureVal = 0
-    userUnit = ""
-    userVal = 0
+    # Request initial values of unit and measurement
+    promptUnit()
+    promptMeasure()
 
-    # Request initial units and measurement
-    while(userUnit == ""):
-        userUnit = input("Which unit of length would you like to use (cm, in, ft, yd, m, km, mi)? Enter 'stop' to quit program:").lower()
-        #for u in lengthUnits:
-        
-    userVal = float(input("Enter the measurement you would like to use in " + userUnit + ":"))
+    while(True):
+        # Reset user input
+        userUnit = ""
+
+        # Request conversion unit
+        promptUnit()
+
+        # Print answer
+        print(str(round(userVal, 3)) + " " + userUnit)
 main()
